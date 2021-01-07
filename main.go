c90215332786534100a6dc3fef4372cd33b132d4
Jonathan Fernandes <int3rlop3r@yahoo.in>
2021-01-08 01:58:02 +0530

    Initial Commit
    Added flag to specify port number
    Added host verification
    Print protocol version

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var port string
+	flag.StringVar(&port, "port", "443", "Port no. of the remote service")
+	flag.Parse()
+	host := flag.Arg(0)
+	if host == "" {
+		fmt.Fprintln(os.Stderr, "hostname cannot be blank")
+		return
+	}
+
+	var config tls.Config
+	addr := fmt.Sprintf("%s:%s", host, port)
+	fmt.Println("connecting to:", addr)
+	conn, err := tls.Dial("tcp", addr, &config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "couldn't connect to host '%s', err: %s\n", addr, err)
+		return
+	}
+
+	err = conn.VerifyHostname(host)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "hostname doesn't match cert, err: %s\n", err)
+		return
+	} else {
+		fmt.Printf("hostname '%s' verified\n", host)
+	}
+	fmt.Println(conn.ConnectionState().Version)
+}
